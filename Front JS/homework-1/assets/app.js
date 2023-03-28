02cb35d3a3612bed7b0345c3d92097e29f8296d1
BehruzRehimli <bahruzkr@code.edu.az>
2023-03-28 23:49:35 +0400

    first homework done

---
+++ Front JS/homework-1/assets/app.js
@@ -0,0 +1,22 @@
+document.getElementById('btn-1').onclick=function(){
+    document.body.style.display= "none";
+}
+document.getElementById('btn-2').onclick=function(){
+    var input = Number(document.getElementById('in-2').value)
+    if(input<18){
+        alert("Diqqət!!! Sayta giriş 18 yaş üzəridir!")
+    }
+}
+document.getElementById('btn-3').onclick = function() {
+    var input = Number(document.getElementById("in-3").value.slice(-3))
+    if (input>99 && input<200) {
+        document.body.style.backgroundColor="yellow";
+    }
+    else if (input>199 && input<300) {
+        document.body.style.backgroundColor="red";
+    }
+    else if (input>299 && input<400) {
+        document.body.style.backgroundColor="grey";
+    }
+};
+
