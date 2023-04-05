f7be4a9593f215d8980c922c20bd318ae6a431c7
BehruzRehimli <bahruzkr@code.edu.az>
2023-04-05 09:38:07 +0400

    done homework-DOM

--- Front JS/homework-16 DOM-ToDoList-Task/assets/app.js
+++ Front JS/homework-16 DOM-ToDoList-Task/assets/app.js
@@ -1,10 +1,17 @@
-let btn = document.querySelector(".input .btn")
+let btn = document.querySelector(".input .btn1")
 let input = document.querySelector(".input .in");
 let list = document.querySelector("#list")
+let alert_primary=document.querySelector(".alert-primary")
+let alert_danger=document.querySelector(".alert-danger")
 
 let counter = 0
 btn.onclick = function () {
+    alert_primary.style.display="inline";
     counter++;
+    setTimeout(() => {
+        alert_primary.style.display="none";
+    }, 2000);
+ 
     if (counter == 1) {
         let x = document.querySelector("#list span")
         x.style.display = "none"
@@ -16,6 +23,10 @@ btn.onclick = function () {
     button.innerHTML = 'Sil'
     content.innerHTML = `${counter}. ${x}`
     button.onclick = () => {
+        alert_danger.style.display="inline";
+        setTimeout(() => {
+            alert_danger.style.display="none";
+        }, 2000);
         div.style.display = "none"
         counter--;
         if (counter == 0) {
